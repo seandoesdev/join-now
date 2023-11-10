30cebd629d9109a798d7b5433c87d86ab23566da
ChoiSeonJun <sjjjang688@gmail.com>
2023-11-10 16:41:48 +0900

    design: 메인페이지, 마이페이지 상단에 프로필이미지 제대로 보이게 만듬. 나머지 페이지 작업 필요..

--- src/main/java/com/exam/controller/ProjTeamController.java
+++ src/main/java/com/exam/controller/ProjTeamController.java
@@ -1,5 +1,7 @@
 package com.exam.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,12 +21,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.amazonaws.auth.SdkClock.Instance;
 import com.exam.dto.MeetingDTO;
 import com.exam.dto.MeetingPageDTO;
 import com.exam.dto.ScheduleDTO;
 import com.exam.dto.TeamDTO;
 import com.exam.dto.TeamMemberDTO;
+import com.exam.dto.UploadDTO;
 import com.exam.dto.UserInfoDTO;
 import com.exam.navercloud.openapi.service.ObjectStorageService;
 import com.exam.service.ProjTeamServiceImpl;
@@ -51,6 +56,9 @@ public class ProjTeamController {
   
   @Autowired
   private UserService userService;
+  
+  @Autowired
+  ObjectStorageService storageService;
 
   // 팀정보
   @GetMapping("/{teamId}")
@@ -78,8 +86,82 @@ public class ProjTeamController {
     model.addAttribute("teamDTO", teamDTO);
     
     
-    return "infoRetrieve";
+    return "introRetrieve";
   }
+  
+  ///////팀 프로필 업로드 ////////////////////////////////////////
+  
+  @PostMapping("/teamupload/{teamId}")
+	public String teamupload(@PathVariable int teamId, UploadDTO dto, Model m, HttpSession session) {
+		
+		//세션에서 id값 받아오기
+		UserInfoDTO userInfoDTO = (UserInfoDTO) session.getAttribute("loginInfo");
+		int id = userInfoDTO.getId();
+		//현재 로그인된 id에 해당하는 정보 받아와서 출력
+		UserInfoDTO info = userService.selectAllById(id);
+		m.addAttribute("UserInfoDTO", info);		
+		
+		//팀정보 가져오기
+		TeamDTO teamDTO = teamService.selectByTeamId(teamId);
+	    m.addAttribute("teamDTO", teamDTO);
+		
+		String theText = dto.getTheText();
+		MultipartFile theFile = dto.getTheFile();
+		
+		//파일정보
+		long size = theFile.getSize();
+		String name = theFile.getName();
+		String originalFilename = theFile.getOriginalFilename();
+		String contentType = theFile.getContentType();
+		
+		
+		System.out.println(theText);
+		System.out.println(size);
+		System.out.println(name);
+		System.out.println(originalFilename);
+		System.out.println(contentType);
+		
+		String teamProfileName = Integer.toString(teamDTO.getTeamId())+".jpg";
+		File f = new File("c:\\upload", teamProfileName) ;
+		
+		try {
+			theFile.transferTo(f);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+
+		
+		// 클라우드에 저장
+		storageService.upload(teamProfileName, "c:\\upload\\"+teamProfileName);
+		
+		// 파일 삭제
+		String filePath = "c:\\upload\\"+teamProfileName;
+      File file = new File(filePath);
+
+      if (file.exists()) {
+          boolean deleted = file.delete();
+          if (deleted) {
+              System.out.println("파일이 삭제되었습니다.");
+          } else {
+              System.out.println("파일 삭제에 실패했습니다.");
+          }
+      } else {
+          System.out.println("파일이 존재하지 않습니다.");
+      }
+		
+		return "redirect:/team/" + teamId;
+	}
+  
+  ///////팀 프로필 업로드 ////////////////////////////////////////
+  
+  
+  
+  
+  
+  
   
   // 팀 소개 수정 작업
   @GetMapping("/update/retrieve.do/{teamId}")
